99916075b2e4e78fc7f42b68b3b6ab5748df1c1d
Angela P. Cuervo-Robayo <ancuervo@gmail.com>
2016-08-24 15:17:18 -0500

    Add files via upload

---
+++ cor2.R
@@ -0,0 +1,37 @@
+
+# ajuste su directorio de trabajo
+setwd('~/Bioclimas');
+
+# set environmental layers to open
+
+variables = c(paste('bio',0,1:9,sep=""),paste('bio',10:19,sep=""));
+
+i = 1;
+
+while (i <= length(variables)) {
+	# elimina las primeras lineas de texto del ascii
+	values = scan(file=paste(variables[i],'.asc',sep=""),skip=6);
+	
+	values[values == -9999] = NA;
+
+	if (i == 1) {
+		df = data.frame(values);
+	} else {
+		df = data.frame(df,values);
+	}
+
+	i = i + 1;
+}
+
+# nombrar las columnas con el nombre de los bioclimas
+names(df) = variables;
+
+corr.matrix = cor(df,use='complete.obs');
+
+write.csv(corr.mtrx,file="corr_matrix.csv",quote=FALSE)
+
+# Plot de las correlaciones
+
+library(corrplot)
+corr.plot <- cor(corr.matrix)
+corrplot(corr.plot, method="circle")
